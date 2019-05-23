00591d57be14e2f229268d0de4b5536c97292e5c
Ellis Cain <50685260+ellissc@users.noreply.github.com>
2019-05-23 12:52:38 -0400

    Add files via upload

--- slice_video.py
+++ slice_video.py
@@ -2,13 +2,13 @@
 import csv
 import os
 
-csvInputFile = 'testValues.csv'
+csvInputFile = 'speech_17275_verbonset_cleaned.csv'
 csvHolder = []
 with open(csvInputFile, newline='') as csvfile:
 	reader = csv.reader(csvfile, delimiter=',', quotechar='|')
 	for row in reader:
 		csvHolder.append(row)
-csvFinal = [[0 for x in range(len(csvHolder))] for y in range(4)]
+csvFinal = [[0 for x in range(4)] for y in range(len(csvHolder))]
 
 counter = 1
 for row in range(0,len(csvHolder)): 
@@ -18,10 +18,11 @@ for row in range(0,len(csvHolder)):
 	template = "{}_{}_{}_{}.mov"
 	prefix = str(csvFinal[row][0]).replace('.mov','')
 	kidID = prefix[-5:]
-	onset = "{:0.2f}".format(float(csvFinal[row][2]))
-	onset_Dash = onset.replace(".","-")
+	#onset = "{:0.2f}".format(float(csvFinal[row][2]))
+	#onset_Dash = onset.replace(".","-")
 	verb = csvHolder[row][2]
-	csvFinal[row][1] = template.format(verb,kidID,counter,onset_Dash) #set file output name
+	#csvFinal[row][1] = template.format(verb,kidID,counter,onset_Dash) #set file output name
+	csvFinal[row][1] = template.format(verb,kidID,counter,"5sec") #set file output name
 	counter= counter+1
 
 
@@ -30,11 +31,13 @@ for row in range(0,len(csvFinal)):
 	timeDiff = csvFinal[row][3] #grabs time difference
 	fileName = csvFinal[row][0] #sets input filename
 	outputPath = csvFinal[row][1] #sets output path
-	command = ["ffmpeg","-ss",str(onsetTime),"-t",str(timeDiff),"-i",str(fileName),"-an",str(outputPath),"-y"]
+	command = ["ffmpeg","-ss",str(onsetTime),"-t",str(timeDiff),"-i",str(fileName),str(outputPath),"-y"]
+	#command = ["ffmpeg","-ss",str(onsetTime),"-t",str(timeDiff),"-i",str(fileName),"-an",str(outputPath),"-y"]
 	command_string = " ".join(command)
 	print(command_string)
 	os.system(command_string)
+	'''
 	beepString = str(outputPath).replace(".mov","_beep.mov")
 	command2 = ["ffmpeg","-i", str(outputPath), "-itsoffset","3","-i","beep2.mp3", "-map","0:0","-map","1:0","-preset","ultrafast",beepString]
 	command_string2 = " ".join(command2)
-	os.system(command_string2)
+	os.system(command_string2)'''
